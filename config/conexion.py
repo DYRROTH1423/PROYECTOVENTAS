a19c9c348cc0ae1c5d92e538cf5ad1d6ecdbd4ee
DYRROTH1423 <naviltucumani2007@cesanrafael.org>
2025-06-06 15:27:04 -0400

    JUAN CARLOS

---
+++ config/conexion.py
@@ -0,0 +1,11 @@
+import  mysql. connector
+
+conexion = mysql. connector.connect(
+    host="localhost",
+    user="root",
+    password="1234",
+    database="tbventas"
+)
+
+if conexion.is_connected():
+    print("Conexión exitosa")
